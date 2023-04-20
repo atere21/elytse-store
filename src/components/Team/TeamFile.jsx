d36ad7331305a6388014c016f79aa51b17be993f
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-04-20 23:36:03 +0100

    Update TeamFile.jsx

--- src/components/Team/TeamFile.jsx
+++ src/components/Team/TeamFile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { team  } from "../../utils/teams";
+import { team  } from "../../utils/teamsCard";
 import TeamCard from "./TeamCard";
 import AOS from "aos";
 import 'aos/dist/aos.css';
