04c4adef3f61482e0210484b4fcfed6d611dfd9a
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-04-20 23:46:16 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Youtube from "../components/Youtube";
 import Faq from "../components/FAQ'S/Faq";
-import Team from "../components/Team/Team";
+import Team from "../components/Team";
 import Services from "../components/Services/MyServices";
 import Hero from '../components/Hero';
 
