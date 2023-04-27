2cde8c2447a14dccc2d11082066b33c1bb34643f
Motun Marteen <111828215+marteenmotun@users.noreply.github.com>
2023-04-27 16:22:45 +0100

    updated faq

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export const data= [
 
     {
-      question:"What is Elytse",
+      question:"What is Elytse?",
       answer: "Elytse is a social fashion market place that connects fashion ethusiasts and fashion conscious shoppers with independent fashion brands, designers and influencers",
   
     },
   
     {
-      question: "How does Elytse Work",
-      answer: "Elytse offer a unique shopping experience that combine social networking with online-shopping. Users can browse and shop for fashion items, connects with like-minded fashion ethusiasts and even earn reward and discount through our gamification features",
+      question: "How does Elytse Works?",
+      answer: "Elytse offer a unique shopping experience that combines social networking with online-shopping. Users can browse and shop for fashion items, connects with like-m    inded fashion ethusiasts and even earn reward and discount through our gamification features",
   
     },
 
     {
       question: "Do you sell only clothes?",
-      answer: "No, we connect sellers of anything fashion from all kinds of wears, jewelries, foot-wears, hairs, accesories like bags and so on..",
+      answer: "No, we connect product and service providers with users who are interested in clothing, beauty and appearance",
   
     },
 
     {
       question: "Do you provide services too?",
-      answer: "Yes, we also coonect service fashion providers like tailors, shoe-makers, models, gold-smith, fashion-photographers, fashion journalists, influencers and so on. Our goal is to provide a platform that offers a diverse selection of fashion services for our users.",
+      answer: "Yes, we also connect service fashion providers like Tailors, Fashion designers, Textile artists, Seamstresses, Costume designers, Mending enthusiasts, Shoe-makers, Cobblers, Models, Make-up artists, Jewelers, Fashion-photographers, Fashion journalists, Logistics personnels, Influencers and so on. Our goal is to provide a platform that offers a diverse selection of fashion services for our users.",
   
     },
     {
-      question: "What are the benefits of using Elytse",
-      answer: "Elytse offers a lot of unique experience that combine social-networking with online shopping. Users can connect with like-minded fashion ethusiasts, discover new independent brands and designers, even make money through sell and swap feature, earn rewards and discounts theough gamification features",
+      question: "What are the benefits of using Elytse?",
+      answer: "Elytse offers a lot of unique experience that combine social-networking with online shopping. Users can connect with like-minded fashion ethusiasts, discover new independent brands and designers, even make money through sell and swap feature, earn rewards and discounts through gamification features",
   
     },
     
     {
-      question: "How can I contact Elytse customer service",
-      answer: "You can contact Elytse customer support through our website or mobile app. We have a dedicated team of customer support representatives that are available to assist you with any questions or concerns",
+      question: "How can I contact Elytse customer service?",
+      answer: "You can contact Elytse customer support through our website or mobile app. We have a dedicated team of customer support representatives that are available to assist you with any questions or concerns.",
   
     },
     ]
