9199340cc472ba54b0315e02a37d79736c75b187
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-04-29 19:21:28 -0600

    Update Vision.jsx

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -27,13 +27,12 @@ export default function Vision() {
       <div className="w-27 bg-[#f9d108] py-12 p-8">
         <h2>Our Vision</h2>
         <p className="p-2">
-          Our Vision at Elytse is to be the leading fashion place in Africa,
-          offering cutting edge technology and services that enable fashion
-          products and service providers to showcase their creation and connect
-          with a diverse range of customers worldwide. We strive to become a
-          global fashion destination that promotes sustainability, creativity
-          and diversity while empowering local communities and driving positive
-          change.
+          At Elytse, our mission is to revolutionize the fashion industry by
+          providing a sustainable, affordable, innovative, and socially-conscious marketplace
+          that connect fashion products and service providers and consumers. We
+          aim to offer a seamless and personalised shopping experience that is
+          accessible to everyone while promoting ethical and eco-friendly
+          fashion practices
         </p>
       </div>
     </div>
